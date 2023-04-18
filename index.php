68d6c5d0f9d780325f9b353dc16964aef975b2ab
KevinZucca <kev.94@live.it>
2023-04-18 15:58:58 +0200

    feat: results in table

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 
 <?php 
-
-   
-
-
+    $hotelNames = [];
+    $hotelDescriptions = [];
+    $hotelParkings = [];
+    $hotelVotes = [];
+    $hotelDistances = [];
 ?>
 
 
@@ -15,6 +16,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php Hotel</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
     <?php
@@ -59,12 +61,90 @@
 
     ];
 
-   foreach($hotels as $hotel) {
-     foreach($hotel as $key => $value){
-        echo "<p> {$key} : {$value} '<br>' </p>" ;
-     }
-   }
+//    foreach($hotels as $hotel) {
+//      foreach($hotel as $key => $value){
+//         echo "<p> {$key} : {$value} '<br>' </p>" ;
+//      }
+//    }
 
+    foreach($hotels as $hotel) {
+        $hotelNames[] = $hotel["name"];
+        $hotelDescriptions[] = $hotel["description"]; 
+        $hotelParkings[] = $hotel["parking"];  
+        $hotelVotes[] = $hotel["vote"]; 
+        $hotelDistances[] = $hotel["distance_to_center"];               
+    }
+
+    var_dump($hotelNames);
+    var_dump($hotelDescriptions);
+    var_dump($hotelParkings);
+    var_dump($hotelVotes);
+    var_dump($hotelDistances);
 ?>
+
+<table class="table table-success table-striped-columns">
+  <thead>
+    <tr>
+    <th>
+    </th>
+    <?php foreach($hotels as $hotel) { ?>
+      <th> 
+        <?php echo $hotel['name'] ?>
+     </th>
+    <?php } ?>
+    </tr>
+    
+  </thead>
+  <tbody>
+  <tr>
+    <td>
+        Description
+    </td>
+    <?php foreach($hotels as $hotel) { ?>
+      <td> 
+        <?php echo $hotel['description'] ?>
+     </td>
+    <?php } ?>
+    </tr>
+
+    <tr>
+    <td>
+        Parking
+    </td>
+    <?php foreach($hotels as $hotel) { ?>
+      <td> 
+        <?php echo $hotel['parking'] == "true" ? "Yes" : "No" ?>
+     </td>
+    <?php } ?>
+    </tr>
+
+    <tr>
+    <td>
+        Vote
+    </td>
+    <?php foreach($hotels as $hotel) { ?>
+      <td> 
+        <?php echo $hotel['vote'] ?>
+     </td>
+    <?php } ?>
+    </tr>
+
+    <tr>
+    <td>
+        Distance to center
+    </td>
+    <?php foreach($hotels as $hotel) { ?>
+      <td> 
+        <?php echo $hotel['distance_to_center'] ?>
+        <span>km</span>
+     </td>
+    <?php } ?>
+    </tr>
+  </tbody>
+</table>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
